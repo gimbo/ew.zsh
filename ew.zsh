f1b787e42b96c21bff44a6ca0fae4bc799c54f42
Andy Gimblett <andy@barefootcode.com>
2017-08-17 10:59:42 +0100

    Initial commit

---
+++ ew.zsh
@@ -0,0 +1,70 @@
+# Emacs for the win!
+
+EMACSTMP=$TMPDIR/emacs502
+
+# Start an emacs server.  Takes one optional argument, the server
+# name, defaulting to "server".
+#
+ess() {
+  if (( $# == 0)); then
+    local server="server"
+  else
+    local server="$1"
+  fi
+  local socket="$EMACSTMP/$server"
+  echo "Looking for emacs socket at $socket"
+  if [ -e "$socket" ]; then
+    echo "Server $server already running"
+  else
+    echo "Starting server $server"
+    emacs --daemon=$server
+  fi
+}
+
+# List the currently running emacs servers, and any emacs processes.
+#
+esl() {
+  ls -a -1 $EMACSTMP | tail +3 | sort
+  echo
+  pgrep -il emacs
+}
+
+# Kill an emacs server.  Takes one optional argument, the server name,
+# defaulting to "server".
+#
+esk() {
+  if (( $# == 0)); then
+    local server="server"
+  else
+    local server="$1"
+  fi
+  local socket="$EMACSTMP/$server"
+  echo "Looking for emacs socket at $socket"
+  if [ -e "$socket" ]; then
+    echo "Sending (kill-emacs) to $server"
+    emacsclient -s $server -e '(kill-emacs)'
+  else
+    echo "Socket not found for server $server, nothing to do"
+  fi
+  sleep 1
+  esl
+}
+
+# Emacs clients for the command line and GUI.
+#
+e() {
+  emacsclient --alternate-editor="" -t "$@"
+}
+ew() {
+  if (( $# == 0 )); then
+    # No filename, just open and focus.
+    emacsclient --alternate-editor="" -c -n --eval '(x-focus-frame nil)' &
+  else
+    emacsclient --alternate-editor="" -c -n "$@" &
+  fi
+}
+
+# We prefer to edit ~/.emacs in its own server space, which
+# automatically gets a different colour.
+#
+alias ewdot="ew -s .emacs ~/.emacs"
